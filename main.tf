05ed38eef2e39f6c308bcbac74e876105350ec03
Ketan Ambure <ambureketan@gmail.com>
2025-10-20 10:45:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web" {
     ami = var.ami_id
     instance_type = var.instance_type
 
-    vpc_security_group_id = [aws_security_group.nginx_sg]
+    vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
     user_data = <<-EOF
                 #!/bin/bash
