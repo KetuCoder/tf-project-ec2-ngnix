9fb54f3e4627726b214328dbbe9d9b7194e3db2b
KetuCoder <ambureketan@gmail.com>
2025-10-20 10:34:32 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+
+    vpc_security_group_ids = [aws_security_group.nginx_sg]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install nginx -y
+                sudo systemctl start nginx
+                sudo systemctl enable nginx
+                EOF
+
+    tags = {
+        Name = "nginx-server"
+    }
+}
+
+resource "aws_security_group" "nginx_sg" {
+    name = "nginx-sg"
+    description = "Allow HTTP and SSH"
+
+    ingress {
+        description = "SSH"
+        from_port = 22
+        to_port = 22
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "HTTP"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "ami_id" {
+    description = "amazon linux ubnutu ami id"
+    default = "ami-0360c520857e3138f"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
